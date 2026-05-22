30e8d59ad6b8c43180791a46d99cba71952adf84
buba <bubaololo@gmail.com>
2026-05-22 13:51:08 +0300

    add docs route to defined

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -45,5 +45,6 @@ class AppServiceProvider extends ServiceProvider
 
         Gate::define('viewHorizon', $isAllowed);
         Gate::define('viewPulse', $isAllowed);
+        Gate::define('viewApiDocs', $isAllowed);
     }
 }
